d96f6eedc007b7b5dc1392a60d6e0655afd41506
Ross Cohen <rosscohen1990@gmail.com>
2024-07-21 22:57:42 -0400

    Update custom_routes.py

--- custom_routes.py
+++ custom_routes.py
@@ -227,7 +227,7 @@ async def comfy_deploy_run(request):
     data = await request.json()
 
     # In older version, we use workflow_api, but this has inputs already swapped in nextjs frontend, which is tricky
-    workflow_api = data.get("workflow_api_raw")
+    workflow_api = data.get("workflow_api")
     # The prompt id generated from comfy deploy, can be None
     prompt_id = data.get("prompt_id")
     inputs = data.get("inputs")
@@ -285,7 +285,7 @@ async def comfy_deploy_run(request):
 
 async def stream_prompt(data):
     # In older version, we use workflow_api, but this has inputs already swapped in nextjs frontend, which is tricky
-    workflow_api = data.get("workflow_api_raw")
+    workflow_api = data.get("workflow_api")
     # The prompt id generated from comfy deploy, can be None
     prompt_id = data.get("prompt_id")
     inputs = data.get("inputs")
