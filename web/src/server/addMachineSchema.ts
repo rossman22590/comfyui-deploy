5acaa7a927f8f45c7075900e23321ed17e07a2e7
Ross Cohen <rosscohen1990@gmail.com>
2024-10-07 15:48:58 -0400

    Update addMachineSchema.ts

--- web/src/server/addMachineSchema.ts
+++ web/src/server/addMachineSchema.ts
@@ -14,15 +14,30 @@ export const insertCustomMachineSchema = createInsertSchema(machinesTable, {
   gpu: (schema) => schema.gpu.default("T4"),
   snapshot: (schema) =>
     schema.snapshot.default({
-      comfyui: "d0165d819afe76bd4e6bdd710eb5f3e571b6a804",
+      comfyui: "ae2dc94a3d75fb9846a11867e48bd73aebbde31a",
       git_custom_nodes: {
-        "https://github.com/BennyKok/comfyui-deploy.git": {
-          hash: "43fe0a384aa5fa9e141d4a264b2ed40a73b817bc",
+        "https://github.com/rossman22590/comfyui-deploy.git": {
+          hash: "ae2dc94a3d75fb9846a11867e48bd73aebbde31a",
           disabled: false,
         },
       },
       file_custom_nodes: [],
     }),
+// export const insertCustomMachineSchema = createInsertSchema(machinesTable, {
+//   name: (schema) => schema.name.default("My Machine"),
+//   type: (schema) => schema.type.default("comfy-deploy-serverless"),
+//   gpu: (schema) => schema.gpu.default("T4"),
+//   snapshot: (schema) =>
+//     schema.snapshot.default({
+//       comfyui: "d0165d819afe76bd4e6bdd710eb5f3e571b6a804",
+//       git_custom_nodes: {
+//         "https://github.com/BennyKok/comfyui-deploy.git": {
+//           hash: "43fe0a384aa5fa9e141d4a264b2ed40a73b817bc",
+//           disabled: false,
+//         },
+//       },
+//       file_custom_nodes: [],
+//     }),
   models: (schema) =>
     schema.models.default([
       {
